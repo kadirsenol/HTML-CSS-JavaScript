39d37b7e5ec62c0b28b5a6117fafe2a0cd7161a2
Kadir ŞENOL <kdrsnl_61@hotmail.com>
2024-02-28 16:58:09 +0300

    personel yonetim web site tasarimi

---
+++ ASP.Net Core Empty/wwwroot/javascript/javaScript.js
@@ -0,0 +1,15 @@
+﻿document.getElementById("persoregister").addEventListener("submit", function (e) {
+    const isim = document.getElementById("isim").value;
+    const soyisim = document.getElementById("soyisim").value;
+    if (isim == "" || soyisim == "") {
+        alert("Lütfen ilgili yerleri doldurunuz.");
+
+        /*buradan devam et kayıt işlemi yaptır*/
+    }
+  
+});
+
+function clear(){
+    document.getElementById("isim").value == "";
+    document.getElementById("soyisim").value == "";
+}
