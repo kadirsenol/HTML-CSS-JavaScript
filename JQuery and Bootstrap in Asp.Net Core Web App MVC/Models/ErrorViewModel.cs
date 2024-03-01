3be49158c084d7f03bf4f300af9846f1ef141c89
Kadir ŞENOL <kdrsnl_61@hotmail.com>
2024-03-02 02:00:06 +0300

    Asp.net core mvc using JQuery and Bootstrap

---
+++ JQuery and Bootstrap in Asp.Net Core Web App MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JQuery_and_Bootstrap_in_Asp.Net_Core_Web_App_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
